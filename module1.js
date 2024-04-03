f36d399444b14658f1b1fbee7874a12075254e60
Haroon <biodunhi@gmail.com>
2024-04-03 22:11:13 +0500

    first commit

---
+++ module1.js
@@ -0,0 +1,5 @@
+// module1.js
+export function sayHello() {
+    console.log("Hello!");
+  }
+  export const pi = 3.14;
---
+++ module2.js
@@ -0,0 +1,4 @@
+// module2.js
+import { sayHello, pi } from './module1.js';
+sayHello(); // Outputs: Hello!
+console.log(pi); // Outputs: 3.14
